126fec62a221a0b150507c347c5fe36841ad09dc
Dmitriy3292 <mr.diy@yandex.ru>
2021-10-29 21:22:57 +0400

    first commit

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+
+    public static void main(String[] args) {
+        int price = 9000;
+        int rub = 20;
+
+        int miles = (price / rub);
+
+        System.out.println("Ваш бонус = " + miles);
+
+    }
+}
